65806be425e36c448c45a5b21d19e89f7294e1cf
Magdy Youssef <magdimedhat@gmail.com>
2017-12-21 10:21:59 +0100

    Initial Codebase

---
+++ phpUnitTutorial/Test/PaymentTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace phpUnitTutorial\Test;
+
+use phpUnitTutorial\Payment;
+
+class PaymentTest extends \PHPUnit_Framework_TestCase
+{
+    public function testProcessPaymentReturnsTrueOnSuccessfulPayment()
+    {
+        $paymentDetails = array(
+            'amount'   => 123.99,
+            'card_num' => '4111-1111-1111-1111',
+            'exp_date' => '03/2013',
+        );
+
+        $payment = new Payment();
+
+        $response = new \stdClass();
+        $response->approved = true;
+        $response->transaction_id = 123;
+
+        $authorizeNet = $this->getMockBuilder('\AuthorizeNetAIM')
+            ->setConstructorArgs(array($payment::API_ID, $payment::TRANS_KEY))
+            ->getMock();
+
+        $authorizeNet->expects($this->once())
+            ->method('authorizeAndCapture')
+            ->will($this->returnValue($response));
+
+        $result = $payment->processPayment($authorizeNet, $paymentDetails);
+
+        $this->assertTrue($result);
+    }
+
+    public function testProcessPaymentThrowsExceptionOnFailure()
+    {
+        $paymentDetails = array(
+            'amount'   => 123.99,
+            'card_num' => '4111-1111-1111-1111',
+            'exp_date' => '03/2013',
+        );
+
+        $payment = new Payment();
+
+        $response = new \stdClass();
+        $response->approved = false;
+        $response->error_message = "invalid card number";
+
+        $authorizeNet = $this->getMockBuilder('\AuthorizeNetAIM')
+            ->setConstructorArgs(array($payment::API_ID, $payment::TRANS_KEY))
+            ->getMock();
+
+        $authorizeNet->expects($this->once())
+            ->method('authorizeAndCapture')
+            ->will($this->returnValue($response));
+
+		$this->setExpectedException(\Exception::class);
+        
+		$payment->processPayment($authorizeNet, $paymentDetails);
+    }	
+}
